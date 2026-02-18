96b2389b0eb864d935515c97971a30c163ff078b
asadjn99 <asad.jn99@gmail.com>
2026-02-18 17:28:26 +0500

    update and remove hardcoded and add env

--- app/src/firebase.ts
+++ app/src/firebase.ts
@@ -1,14 +1,40 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAG8FcQAzEqqz-AF9F8RJnEJUQJotfT42A",
+//   authDomain: "eb-dashboard-9f41e.firebaseapp.com",
+//   projectId: "eb-dashboard-9f41e",
+//   storageBucket: "eb-dashboard-9f41e.firebasestorage.app",
+//   messagingSenderId: "769245649412",
+//   appId: "1:769245649412:web:d064a9d71f6c06d61db351"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+// export const auth = getAuth(app);
+
+
+
+
+
+
+
+
+
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAG8FcQAzEqqz-AF9F8RJnEJUQJotfT42A",
-  authDomain: "eb-dashboard-9f41e.firebaseapp.com",
-  projectId: "eb-dashboard-9f41e",
-  storageBucket: "eb-dashboard-9f41e.firebasestorage.app",
-  messagingSenderId: "769245649412",
-  appId: "1:769245649412:web:d064a9d71f6c06d61db351"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
